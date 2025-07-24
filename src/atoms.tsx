774967e5b033783d53562bab2eb76cfade9fcc92
Ahn Honghyun <anh2073@naver.com>
2025-07-24 16:03:42 +0900

    Update hourSelector

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -4,3 +4,11 @@ export const minuteState = atom({
   key: "minutes",
   default: 0,
 });
+
+export const hourSelector = selector({
+  key: "hours",
+  get: ({ get }) => {
+    const minutes = get(minuteState);
+    return minutes / 60;
+  },
+});
