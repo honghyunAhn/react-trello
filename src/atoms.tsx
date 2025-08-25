9c4c07ff24c4f06e108353a91187d875075c4562
Ahn Honghyun <anh2073@naver.com>
2025-08-25 18:02:38 +0900

    Cross Board Movement

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -8,7 +8,8 @@ export const toDoState = atom<IToDoState>({
   key: "toDo",
   default: {
     "To Do": ["a", "b"],
-    doing: ["c", "d", "e"],
-    done: ["f"],
+    Doing: ["c", "d", "e"],
+    Done: ["f"],
+    "Do Later": ["x", "z"],
   },
 });
