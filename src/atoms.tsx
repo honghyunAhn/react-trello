3f808bb37b32ea53cd8133a8dba741fcf6c46dea
Ahn Honghyun <anh2073@naver.com>
2025-09-25 11:03:10 +0900

    Task Objects

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,15 +1,37 @@
-import { atom } from "recoil";
+// 7.14 Task Objects
+// import { atom } from "recoil";
 
-interface IToDoState {
-  [key: string]: string[];
+// export interface ITodo {
+//   id: number;
+//   text: string;
+// }
+// interface IToDoState {
+//   [key: string]: ITodo[];
+// }
+
+// export const toDoState = atom<IToDoState>({
+//   key: "toDo",
+//   default: {
+//     "To Do": [],
+//     Doing: [],
+//     Done: [],
+//   },
+// });
+
+// final
+import { atom } from "recoil";
+import { loadToDos } from "./local";
+
+export interface IToDo {
+  id: number;
+  text: string;
+}
+
+export interface IToDoState {
+  [key: string]: IToDo[];
 }
 
 export const toDoState = atom<IToDoState>({
   key: "toDo",
-  default: {
-    "To Do": ["a", "b"],
-    Doing: ["c", "d", "e"],
-    Done: ["f"],
-    "Do Later": ["x", "z"],
-  },
+  default: loadToDos() ?? {},
 });
