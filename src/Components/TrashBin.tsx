ffd77faf83da3c1b127d67b2cd20195dfce1f70e
Ahn Honghyun <anh2073@naver.com>
2025-10-02 09:45:40 +0900

    TrashBin

--- src/Components/TrashBin.tsx
+++ src/Components/TrashBin.tsx
@@ -22,3 +22,23 @@ const Area = styled.div<IAreaProps>`
   transition: background-color 0.2s ease-in-out;
   pointer-events: none;
 `;
+function TrashBin() {
+  return (
+    <Droppable droppableId="trash-bin">
+      {(magic, info) => (
+        <>
+          <Area
+            $isDraggingOver={info.isDraggingOver}
+            ref={magic.innerRef}
+            {...magic.droppableProps}
+          >
+            <span>🗑️</span>
+            {/* {magic.placeholder ? null : null} */}
+          </Area>
+        </>
+      )}
+    </Droppable>
+  );
+}
+
+export default TrashBin;
