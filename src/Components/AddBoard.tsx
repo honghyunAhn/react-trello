5b595a38583c9d8b77e563739d312c4c122f3c7e
Ahn Honghyun <anh2073@naver.com>
2025-10-03 15:27:11 +0900

    Add Board Wrapper

---
+++ src/Components/AddBoard.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 4px;
+  align-items: center;
+  text-align: center;
+  margin-top: 10px;
+`;
