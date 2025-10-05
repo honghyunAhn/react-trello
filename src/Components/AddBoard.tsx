4b72dca888de3a4063c4876628541a276bcc4726
Ahn Honghyun <anh2073@naver.com>
2025-10-06 08:48:06 +0900

    Add Board Box

--- src/Components/AddBoard.tsx
+++ src/Components/AddBoard.tsx
@@ -28,3 +28,29 @@ const ToggleBtn = styled.div`
     transition: background-color 0.1s ease-in-out;
   }
 `;
+
+const AddBoardBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: ${(props) => props.theme.boardColor};
+  color: ${(props) => props.theme.textColor};
+  border: none;
+  width: 300px;
+  padding: 10px;
+  font-size: 18px;
+  font-weight: 600;
+  border-radius: 10px;
+  input {
+    border-radius: 5px;
+    outline: none;
+    box-shadow: 0 0 0 1px rgba(255, 255, 255, 0.4) inset;
+    background-color: ${(props) => props.theme.cardColor};
+    border: none;
+    padding: 10px;
+    color: ${(props) => props.theme.textColor};
+    width: 100%;
+    &:focus {
+      box-shadow: 0 0 0 2px #5b8ac7 inset;
+    }
+  }
+`;
