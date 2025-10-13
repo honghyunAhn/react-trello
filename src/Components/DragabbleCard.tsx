5cce90e164e9d77785cc375d488015cc441e5f53
Ahn Honghyun <anh2073@naver.com>
2025-10-13 17:18:51 +0900

    Update DragabbleCard

--- src/Components/DragabbleCard.tsx
+++ src/Components/DragabbleCard.tsx
@@ -1,32 +1,23 @@
-import { memo } from "react";
+import React from "react";
 import { Draggable } from "react-beautiful-dnd";
 import styled from "styled-components";
 
-const Card = styled.div<{ isDragging: boolean }>`
-  border-radius: 5px;
-  margin-bottom: 5px;
-  padding: 10px 10px;
-  background-color: ${(props) =>
-    props.isDragging ? "#74b9ff" : props.theme.cardColor};
-  box-shadow: ${(props) =>
-    props.isDragging ? "0px 2px 5px rgba(0,0,0,0.5)" : "none"};
-`;
+const Card = styled.div``;
 
-interface IDragabbleCardProps {
+interface IDraggableCardProps {
   toDoId: number;
   toDoText: string;
   index: number;
 }
-
-function DragabbleCard({ toDoId, toDoText, index }: IDragabbleCardProps) {
+function DraggableCard({ toDoId, toDoText, index }: IDraggableCardProps) {
   return (
     <Draggable draggableId={toDoId + ""} index={index}>
       {(magic, snapshot) => (
         <Card
-          isDragging={snapshot.isDragging}
+          className={snapshot.isDragging ? "dragging" : ""}
           ref={magic.innerRef}
-          {...magic.dragHandleProps}
           {...magic.draggableProps}
+          {...magic.dragHandleProps}
         >
           {toDoText}
         </Card>
@@ -35,4 +26,4 @@ function DragabbleCard({ toDoId, toDoText, index }: IDragabbleCardProps) {
   );
 }
 
-export default memo(DragabbleCard);
+export default React.memo(DraggableCard);
